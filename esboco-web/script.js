ca2883ee045c9419b2b1cf5358a7fecc90bdd002
João Sucupira <jasucupira@gmail.com>
2024-02-23 11:02:24 -0300

    Ajustando a barra de menu superior

---
+++ esboco-web/script.js
@@ -0,0 +1,4 @@
+document.getElementByType("search").addEventListener("click", function() {
+    this.style.color = "white";
+    this.style.backgroundColor = "black";
+});
--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.getElementById('login-link').addEventListener('click', function() {
     var loginBox = document.getElementById('login-box');
     loginBox.style.display = (loginBox.style.display === 'block') ? 'none' : 'block';
 });
+
