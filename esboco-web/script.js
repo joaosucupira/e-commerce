02a0efd4852dddd7e87be441f0f891937e7b2282
João Sucupira <jasucupira@gmail.com>
2024-02-23 14:04:35 -0300

    Configurei os titulos para as seções de página

--- esboco-web/script.js
+++ esboco-web/script.js
@@ -1,4 +1,20 @@
-document.getElementByType("search").addEventListener("click", function() {
-    this.style.color = "white";
-    this.style.backgroundColor = "black";
+
+// Texto a ser digitado
+const text = "Qualidade, variedade e frete grátis.";
+
+// Função para adicionar letra por letra ao elemento HTML
+function typeWriter(text, i, typingElement) {
+  if (i < text.length) {
+    typingElement.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(function() {
+      typeWriter(text, i, typingElement);
+    }, 100); // Intervalo de 100ms entre cada letra
+  }
+}
+
+// Chamada da função ao carregar a página
+document.addEventListener("DOMContentLoaded", function() {
+  const typingElement = document.getElementById("typing-text");
+  typeWriter(text, 0, typingElement);
 });
