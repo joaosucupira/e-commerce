3b1aeabd633bcac8a13636aec074085b8b74af68
João Sucupira <jasucupira@gmail.com>
2024-04-19 13:04:44 -0300

    Attempt to change page deployed.

--- esboco/script.js
+++
@@ -1,8 +0,0 @@
-let username;
-
-// getElementById e textContent
-
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Oops, ainda não programaram isso =P`;
-}
--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('login-link').addEventListener('click', function() {
-    var loginBox = document.getElementById('login-box');
-    loginBox.style.display = (loginBox.style.display === 'block') ? 'none' : 'block';
-});
-
